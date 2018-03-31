8e60e6141f9e016efd0c5394edb9be4b01957d73
Harsh Kedia <harshkedia05@gmail.com>
2018-03-31 17:54:59 +0200

    Watch event

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -15,6 +15,11 @@ contract Election {
   // Store Candidates Count
   uint public candidatesCount;
 
+  // voted event
+  event votedEvent (
+    uint indexed _candidateId
+  );
+
   function Election () public {
     addCandidate("Candidate 1");
     addCandidate("Candidate 2");
@@ -35,5 +40,8 @@ contract Election {
     
     // update candidate vote count
     candidates[_candidateId].voteCount ++;
+
+    // trigger voted event
+    votedEvent(_candidateId);
   }
 }
