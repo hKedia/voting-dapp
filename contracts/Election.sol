46032007e7f212dc323c217077b676020224badf
Harsh Kedia <harshkedia05@gmail.com>
2018-03-31 17:35:33 +0200

    Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,7 +7,8 @@ contract Election {
     string name;
     uint voteCount;
   }
-
+  // Store accounts that have voted
+  mapping(address => bool) public voters;
   // Store Candidates
   // Fetch Candidate
   mapping(uint => Candidate) public candidates;
@@ -22,5 +23,17 @@ contract Election {
   function addCandidate (string _name) private {
     candidatesCount ++;
     candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+  }
+
+  function vote(uint _candidateId) public {
+    // require that they haven't voted
+    require(!voters[msg.sender]);
+    // require a valid candidate
+    require(_candidateId > 0 && _candidateId <= candidatesCount);
+    // record that voter has voted
+    voters[msg.sender] = true;
+    
+    // update candidate vote count
+    candidates[_candidateId].voteCount ++;
   }
 }
